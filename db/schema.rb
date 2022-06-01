53fbcc513a86edfbc95671666de6dd1275fd7ebd
Leandro Rosso <lrosso.ti@gmail.com>
2022-06-01 12:39:29 -0300

    Atualizando após finalizada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_135709) do
+ActiveRecord::Schema.define(version: 2022_05_31_184459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_135709) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
   add_foreign_key "comments", "posts"
